7048ce8a3af313aec235994aa954190d991d6b27
HennadiiKondratiuk <henadiy.kondratiuk@gmail.com>
2023-03-08 10:45:44 +0200

    condition to rouye table

--- network/main.tf
+++ network/main.tf
@@ -81,10 +81,13 @@ resource "aws_subnet" "private_subnets" {
 resource "aws_route_table" "private_subnets" {
   count  = length(var.private_subnet_cidrs)
   vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat[count.index].id
-  }
+  var.nat ? route {
+                cidr_block = "0.0.0.0/0"
+                gateway_id = aws_nat_gateway.nat[count.index].id
+            } : route {
+                    cidr_block = "0.0.0.0/0"
+                }
+
   tags = {
     Name = "${var.env}-route-private-subnet-${count.index + 1}"
   }
