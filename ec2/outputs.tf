36f4f6f99fbaf63cc9b85a0678fd8eac34b0039f
HennadiiKondratiuk <henadiy.kondratiuk@gmail.com>
2023-03-13 12:30:34 +0200

    outputs fix

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -6,8 +6,8 @@ output "key_name" {
   value = aws_key_pair.key_pair.key_name
 }
 output "public_ip" {
-  value = aws_instance.ec2.public_ip
+  value = aws_instance.ec2.*.public_ip
 }
 output "private_ip" {
-  value = aws_instance.ec2.private_ip
+  value = aws_instance.ec2.*.private_ip
 }
